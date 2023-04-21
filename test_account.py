52ce771fd4c6aa0fb472780ac812c5082b93e359
LPSOE <lahpwemusoe@gmail.com>
2023-04-21 16:54:26 -0500

    Added Second File

---
+++ test_account.py
@@ -0,0 +1,18 @@
+
+from account import *
+
+class Test:
+
+    def setup_method(self):
+        self.a1 = Account('John')
+    def teardown_method(self):
+        def self.a1
+    def test_init(self):
+
+        pass
+    def test_deposit(self):
+        # negative, zero, positive
+        pass
+    def test_withdraw(self):
+        # negative, zero, positive invalid, positive valid
+        pass
