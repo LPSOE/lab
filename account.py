a13cc6b3ee5417e6c872936fbc9eb395390c484e
LPSOE <lahpwemusoe@gmail.com>
2023-04-21 17:50:08 -0500

    Made changes to account file

--- account.py
+++ account.py
@@ -1,16 +1,37 @@
 class Account:
 
-    def __init__(self, name):
-        pass
+    def __init__(self, name:str) -> None:
+        """
+        function to set up account object.
+        ...
+        :param name: Account name
+        """
+        self.__account_name = "John"
+        self.__account_balance = 0
 
-    def deposit(self, amount):
-        pass
+    def deposit(self, amount) -> bool:
+        """
+        function to set up deposit object
+        :param amount: cannot deposit negative amount of money
+        :return: False
 
-    def withdraw(self, amount):
-        pass
+        """
+        if amount <= 0:
+            return False
+        else:
+            self.__account_balance += amount
+            return True
 
-    def balance(self):
-        pass
+
+    def withdraw(self, amount) -> bool:
+        if amount <= 0 or amount >= self.__account_balance:
+            return False
+        else:
+            self.__account_balance -= amount
+            return True
+
+    def get_balance(self):
+        return self.__account_balance
 
     def get_name(self):
-        pass
+        return self.__account_name
