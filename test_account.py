26255c72b7b707cdd3f6013aa55087eba2d6df8f
LPSOE <lahpwemusoe@gmail.com>
2023-04-21 18:02:10 -0500

    Made changes to account file

--- test_account.py
+++ test_account.py
@@ -1,18 +1,40 @@
-from pytest import *
+import pytest
 from account import *
 
 class Test:
 
     def setup_method(self):
         self.a1 = Account('John')
+
     def teardown_method(self):
         del self.a1
-    def test_init(self):
 
-        pass
+    def test_init(self):
+        assert self.a1.get_name() == 'John'
+
     def test_deposit(self):
         # negative, zero, positive
-        pass
+        assert self.a1.deposit(-1.5) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(1.5) is True
+        assert self.a1.get_balance() == pytest.approx(1.5, abs=0.01)
+
     def test_withdraw(self):
         # negative, zero, positive invalid, positive valid
-        pass
+
+        assert self.a1.withdraw(-1.5) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.withdraw(0) is False
+        assert self.a1.get_balance() == 0
+
+        self.a1.deposit(10)
+        assert self.a1.withdraw(20) is False
+        assert self.a1.get_balance() == 10
+
+        assert self.a1.withdraw(1.5) is True
+        assert self.a1.get_balance() == pytest.approx(8.5)
