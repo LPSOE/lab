2351481cccb2c68782061b0f2157e5761642b48a
LPSOE <lahpwemusoe@gmail.com>
2023-04-21 16:41:28 -0500

    Added first file

---
+++ account.py
@@ -0,0 +1,16 @@
+class Account:
+
+    def __init__(self, name):
+        pass
+
+    def deposit(self, amount):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def balance(self):
+        pass
+
+    def get_name(self):
+        pass
